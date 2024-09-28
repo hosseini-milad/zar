52e4c0b0f758d56511e74d4191be8b86f5a7c879
milad.cia <milad.cia@gmail.com>
2024-09-28 11:46:37 +0330

    cart fix

--- middleware/CalcCart.js
+++ middleware/CalcCart.js
@@ -1,5 +1,6 @@
 const customers = require("../models/auth/customers");
 const cart = require("../models/product/cart");
+const products = require("../models/product/products");
 var ObjectID = require('mongodb').ObjectID;
 
 const CalcCart=async(userId)=>{
@@ -9,8 +10,9 @@ const CalcCart=async(userId)=>{
     const cartDetails = await cart.find({userId:userId}).lean()
     for(var c=0;c<cartDetails.length;c++){
     //const ItemId = 
-    
-    totalCount += parseInt(cartDetails[c].count)
+    cartDetails[c].productDetail = 
+        await products.findOne({sku:cartDetails[c].sku})
+        totalCount += parseInt(cartDetails[c].count)
     }
     return({cart:cartDetails,
         cartDetail: {
