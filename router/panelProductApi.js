c5ee74ddb19a627ba5fa328640c15e4398ba876e
milad.cia <milad.cia@gmail.com>
2024-09-29 14:43:41 +0330

    image upload

--- router/panelProductApi.js
+++ router/panelProductApi.js
@@ -31,6 +31,7 @@ const users = require('../models/auth/users');
 const products = require('../models/product/products');
 const UpdateMarket = require('../middleware/UpdateMarket');
 const crmlist = require('../models/crm/crmlist');
+const multer = require('multer');
 
 router.post('/fetch-service',jsonParser,async (req,res)=>{
     var serviceId = req.body.serviceId?req.body.serviceId:''
@@ -742,4 +743,40 @@ router.post('/report-total',jsonParser,auth,async(req,res)=>{
         res.status(500).json({message: error.message})
     }
 })
+
+var storage = multer.diskStorage(
+    {
+        destination: '/dataset/',
+        filename: function ( req, file, cb ) {
+            cb( null, "Deep"+ '-' + Date.now()+ '-'+file.originalname);
+        }
+    }
+  );
+  const uploadImg = multer({ storage: storage ,
+    limits: { fileSize: "5mb" }})
+
+    router.post('/upload',uploadImg.single('upload'), async(req, res, next)=>{
+        const folderName = req.body.folderName?req.body.folderName:"temp"
+        try{
+        // to declare some path to store your converted image
+        var matches = req.body.base64image.match(/^data:([A-Za-z-+/]+);base64,(.+)$/),
+        response = {};
+        if (matches.length !== 3) {
+        return new Error('Invalid input string');
+        }
+        response.type = matches[1];
+        response.data = new Buffer.from(matches[2], 'base64');
+        let decodedImg = response;
+        let imageBuffer = decodedImg.data;
+        //let type = decodedImg.type;
+        //let extension = mime.extension(type);
+        let fileName = `Zar-${Date.now().toString()+"-"+req.body.imgName}`;
+       var upUrl = `/upload/${folderName}/${fileName}`
+        fs.writeFileSync("."+upUrl, imageBuffer, 'utf8');
+        return res.send({"status":"success",url:upUrl});
+        } catch (e) {
+            res.send({"status":"failed",error:e});
+        }
+    })
+
 module.exports = router;
