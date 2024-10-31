e8c70d6508ff885164b60ed368ace71008fe4067
Amir Hossein Salimi <ah.salimi1431@gmail.com>
2024-10-31 14:17:55 +0330

    new changes

--- src/modules/Crm/ProductPopUp.js
+++ src/modules/Crm/ProductPopUp.js
@@ -37,7 +37,7 @@ function ProductPopUp(props){
                 </div>
                 <i className="fa fa-remove closeModal" 
                     onClick={props.close}></i>
-                <div className="sharif" style={{padding: "70px 10px 10px"}}>
+                <div className="sharif pop-sharif" style={{padding: "70px 10px 10px"}}>
                     <main className="sharif-order-main">
                         <section className="admin-table-sec single-product-table">
                             <table>
