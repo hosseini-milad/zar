8d5834a7f0746cd12b492786cd373983fbf7291c
milad.cia <milad.cia@gmail.com>
2024-11-02 14:16:23 +0330

    changes

--- router/panelUserApi.js
+++ router/panelUserApi.js
@@ -146,7 +146,7 @@ router.post('/fetch-customer',jsonParser,async (req,res)=>{
     var userId = req.body.userId
     try{
         const userData = await customer.findOne({_id: ObjectID(userId)})
-        const groupList = await customer.distinct('group','groupCode')
+        const groupList = await customer.distinct('group',{})
        res.json({data:userData,groupList})
     }
     catch(error){
