bbfde7beebefc56f45c4db5036497168d1d408e3
milad.cia <milad.cia@gmail.com>
2024-11-11 12:06:23 +0330

    mehvar

--- middleware/CalcCart.js
+++ middleware/CalcCart.js
@@ -61,22 +61,28 @@ const CalcCart=async(userId,remainRaw,manageId)=>{
                     }
                 ]
             },
-            {title:"سکه تمام بهار",id:10,unitPrice:unitPrice,
-                ayar:"740",weight:"4.06",
-                parameters:[
-                    {title:"تعداد",value:"count",icon:"fa-comment",
-                        options:[1,2,3,4,5,6,7,8,9,10]
+            {title:"خرید سکه",id:10,unitPrice:unitPrice,
+                isCoin:1,
+                options:[
+                    {title:"سکه تمام بهار",id:10,unitPrice:unitPrice,
+                        ayar:"740",weight:"4.06",
+                        parameters:[
+                            {title:"تعداد",value:"count",icon:"fa-comment",
+                                options:[1,2,3,4,5,6,7,8,9,10]
+                            }
+                        ]
+                    },
+                    {title:"سکه نیم بهار",id:11,unitPrice:unitPrice,
+                        ayar:"740",weight:"2.03",
+                        parameters:[
+                            {title:"تعداد",value:"count",icon:"fa-comment",
+                                options:[1,2,3,4,5,6,7,8,9,10]
+                            }
+                        ]
                     }
                 ]
             },
-            {title:"سکه نیم بهار",id:11,unitPrice:unitPrice,
-                ayar:"740",weight:"2.03",
-                parameters:[
-                    {title:"تعداد",value:"count",icon:"fa-comment",
-                        options:[1,2,3,4,5,6,7,8,9,10]
-                    }
-                ]
-            }
+            
         ],
         faktorData
     })
