00a8dc46a1ea5d080c5221baf606bb1eb29b322d
milad <milad.cia@gmail.com>
2024-11-12 17:48:34 +0330

    sale

--- middleware/CalcPurchase.js
+++ middleware/CalcPurchase.js
@@ -1,13 +1,15 @@
+const FloatDec = require("./FloatDec")
 
 const CalcPurchase=(Ayar,price,weight,count)=>{
     if(!weight||!Ayar) return(0)
     var floatWeight = parseFloat(weight.replace(/\//g,'.'))
     var ayarPercent = parseFloat(Ayar)/750
     var ayarRound = Math.round(floatWeight*ayarPercent*100)/100
-    var finalPrice = ayarRound*price
+    var finalPrice = FloatDec(ayarRound*price,1)
     
     var priceDetail = {
-        Ayar:Ayar, ayarPercent:ayarRound, singlePrice:finalPrice,
+        Ayar:Ayar, ayarPercent:ayarRound, 
+        singlePrice:finalPrice,
         singleWeight:floatWeight,
         unitPrice:price, weight:floatWeight*count,
         roundPrice:finalPrice*count
