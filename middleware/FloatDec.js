2315f602a6f08b10322712b4de490949b557e69e
milad <milad.cia@gmail.com>
2024-12-02 16:21:40 +0330

    cart

--- middleware/FloatDec.js
+++ middleware/FloatDec.js
@@ -1,5 +1,5 @@
 const FloatDec=(number,decRaw)=>{
-    var dec = decRaw?(decRaw^10):100
+    var dec = decRaw?(10^decRaw):100
     var finalNumber = 0
     finalNumber = parseInt(number*dec)/dec
     return(finalNumber)
