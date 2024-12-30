7b49dc6fb412f578c81c904b13232f3c8fce35d8
milad.cia <milad.cia@gmail.com>
2024-12-30 11:43:52 +0330

    remain

--- middleware/Calc/FindRemainUser.js
+++ middleware/Calc/FindRemainUser.js
@@ -1,7 +1,10 @@
+const transaction = require("../../models/param/transaction")
+const cart = require("../../models/product/cart")
+const FindRemainBank = require("./FindRemainBank")
 
 const FindRemainUser=async(userId)=>{
         const cartDetails = await cart.find({userId:userId})
-        var transData = userId?await transactions.find({userId:userId,orderNo:{$exists:false}}):''
+        var transData = userId?await transaction.find({userId:userId,orderNo:{$exists:false}}):''
         var transRemain = FindRemainBank(cartDetails,transData)
         return(transRemain)
 }
