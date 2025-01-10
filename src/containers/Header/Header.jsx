1fd60c37075d6fd12ac92b8acaaa63b2723e975e
Amir Hossein Salimi <ah.salimi1431@gmail.com>
2025-01-10 16:22:26 +0330

    responsive

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 export default function Header() {
   const NavList = [
@@ -11,12 +11,16 @@ export default function Header() {
     { title: "راهنما", link: "#" },
     { title: "فارسی", link: "#" },
   ];
+  const [SubMenu, setSubMenu] = useState(false);
   return (
     <header className="header">
       <div className="container">
         <div className="wrapper logo-wrapper">
           <a href="/">
-            <img src="/Logo.png" alt="Logo" />
+            <picture>
+              <source media="(max-width:750px)" srcset="/logoRaw.png" />
+              <img src="/Logo.png" alt="Logo" />
+            </picture>
           </a>
         </div>
         <div className="wrapper search-wrapper">
@@ -55,40 +59,42 @@ export default function Header() {
           </div>
         </div>
         <div className="wrapper mobile-wrapper">
-          <i class="fa-solid fa-bars"></i>
-          <div className="sub-menu">
-            <ul>
-              {NavList.map((nav, n) => (
-                <li key={n} className="nav-item">
-                  <a href={nav.link}>
-                    <p>{nav.title}</p>
-                  </a>
-                </li>
-              ))}
-            </ul>
-            <div className="wrapper ul-wrapper">
-              <div className="ul-item">
-                <i className="fa-solid fa-circle-user"></i>
-                <div className="p-wrapper">
-                  <p>
-                    <span className="hover">ورود</span>
-                    <span> / </span>
-                    <span className="hover">پیوستن</span>
-                  </p>
-                  <p>حساب کاربری</p>
+          <i class="fa-solid fa-bars" onClick={() => setSubMenu(!SubMenu)}></i>
+          {SubMenu && (
+            <div className="sub-menu">
+              <ul>
+                {NavList.map((nav, n) => (
+                  <li key={n} className="nav-item">
+                    <a href={nav.link}>
+                      {nav.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+              <div className="wrapper ul-wrapper">
+                <div className="ul-item">
+                  <i className="fa-solid fa-circle-user"></i>
+                  <div className="p-wrapper">
+                    <p>
+                      <span className="hover">ورود</span>
+                      <span> / </span>
+                      <span className="hover">پیوستن</span>
+                    </p>
+                    <p>حساب کاربری</p>
+                  </div>
                 </div>
-              </div>
-              <div className="ul-item">
-                <i className="fa-solid fa-envelope auto-hover"></i>
-                <div className="p-wrapper">
-                  <p>
-                    <span className="auto-hover">اعلانات</span>
-                  </p>
-                  <p className="auto-hover">پیام ها</p>
+                <div className="ul-item">
+                  <i className="fa-solid fa-envelope auto-hover"></i>
+                  <div className="p-wrapper">
+                    <p>
+                      <span className="auto-hover">اعلانات</span>
+                    </p>
+                    <p className="auto-hover">پیام ها</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </header>
