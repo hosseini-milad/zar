2612d9b7112a2dc32268d1eb04a95421e631fe1d
milad.cia <milad.cia@gmail.com>
2025-02-03 10:02:30 +0330

    old

--- middleware/Calc/FindSimilarProduct.js
+++ middleware/Calc/FindSimilarProduct.js
@@ -53,11 +53,11 @@ for(var i=0;i<totalData.length;i++){
         }
 }
 var finalDuplicate = []
-if(duplicates&&duplicates.length>3){
-        var dup1 = duplicates[1]
+if(uniqueElements&&uniqueElements.length>3){
+        var dup1 = uniqueElements[1]
         if(!dup1.thumbUrl) dup1.thumbUrl="https://admin.barzegargold.com/upload/default/product.png"
         if(!dup1.imageUrl) dup1.imageUrl="https://admin.barzegargold.com/upload/default/product.png"
-        var dup2 = duplicates[duplicates.length-1]
+        var dup2 = uniqueElements[uniqueElements.length-1]
         if(!dup2.thumbUrl) dup2.thumbUrl="https://admin.barzegargold.com/upload/default/product.png"
         if(!dup2.imageUrl) dup2.imageUrl="https://admin.barzegargold.com/upload/default/product.png"
         
