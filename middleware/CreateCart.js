3a7badebd31fcb38d0a74ac408869abcffe7d06c
milad <milad.cia@gmail.com>
2025-02-25 11:52:19 +0330

    darsad

--- middleware/CreateCart.js
+++ middleware/CreateCart.js
@@ -29,7 +29,7 @@ const CreateCart=async(cartDetails,sku,userId)=>{
             ayar:productDetail.ayar,
             priceDetail:priceDetail.priceDetail,
             price:mojood?price:parseFloat(PRE&&PRE.percent)*price/100,
-            fullPrice:price,
+            fullPrice:FloatDec(price),
             unitPrice:priceRaw,
             isMojood:productDetail.isMojood,
             isReserve:mojood?0:1,
