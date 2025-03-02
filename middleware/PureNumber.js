242c837c3f6a6fdc3b3eec5e21271dd0eb82fa72
milad.cia <milad.cia@gmail.com>
2025-03-02 16:02:04 +0330

    replace

--- middleware/PureNumber.js
+++ middleware/PureNumber.js
@@ -1,7 +1,7 @@
 var tax = process.env.TaxRate
 
 const PureNumber=(number)=>{
-    var result = parseInt(number.replace(/\D/g,''))
+    var result = parseInt(number&&number.replace(/\D/g,''))
     return(result)
 }
 
