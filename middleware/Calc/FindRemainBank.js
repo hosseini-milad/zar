e9745f8b864a3d43a2d9a0a2fd816888f5d29f9d
milad <milad.cia@gmail.com>
2025-03-09 15:08:41 +0330

    catCode

--- middleware/Calc/FindRemainBank.js
+++ middleware/Calc/FindRemainBank.js
@@ -12,7 +12,7 @@ const FindRemainBank=(cartDetails,transData)=>{
                 if(cartDetails[i].purchase)
                         totalPrice-= NormalNumber(cartDetails[i].fullPrice)
                 else
-                        totalPrice+= NormalNumber(cartDetails[i].cartDetail.finalPrice)
+                        totalPrice+= NormalNumber(cartDetails[i].finalPrice)
         }
         var remain = totalPrice-totalPay
         return({totalPay,remain,totalPrice})
