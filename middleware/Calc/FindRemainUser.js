954d8b5ce3e1f464ff737f47ddce6800108e278b
milad.cia <milad.cia@gmail.com>
2025-03-10 17:24:57 +0330

    faktor

--- middleware/Calc/FindRemainUser.js
+++ middleware/Calc/FindRemainUser.js
@@ -1,12 +1,14 @@
 const transaction = require("../../models/param/transaction")
 const cart = require("../../models/product/cart")
+const CalcCart = require("../CalcCart")
 const FindRemainBank = require("./FindRemainBank")
 
 const FindRemainUser=async(userId)=>{
-        const cartDetails = await cart.find({userId:userId})
+        const cartDetails = await CalcCart(userId)
         var transData = userId?await transaction.find({userId:userId,orderNo:{$exists:false}}):''
-        
-        var transRemain = FindRemainBank(cartDetails,transData)
+        var payDetail = cartDetails.cartDetail
+        var totalPay = payDetail&&payDetail.finalPrice
+        var transRemain = FindRemainBank(transData,totalPay)
         return({...transRemain,transData})
 }
 
