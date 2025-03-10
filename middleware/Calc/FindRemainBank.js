faae474041b38c696c884b34b242df12246fa530
milad.cia <milad.cia@gmail.com>
2025-03-10 17:20:31 +0330

    faktor

--- middleware/Calc/FindRemainBank.js
+++ middleware/Calc/FindRemainBank.js
@@ -1,13 +1,11 @@
 const NormalNumber = require("../NormalNumber")
 
-const FindRemainBank=(transData,totalPay)=>{
-        console.log(totalPay)
+const FindRemainBank=(transData,totalPrice)=>{
         var totalPay = 0
         for(var i=0;i<(transData&&transData.length);i++){
                 //console.log(transData[i])
                 totalPay+= NormalNumber(transData[i].payValue)
         }
-        const totalPrice = totalPay
         var remain = totalPrice-totalPay
         return({totalPay,remain,totalPrice})
         
