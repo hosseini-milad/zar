bb27db279a368e9624b5c02b9ef8d1659c00fc0c
milad.cia <milad.cia@gmail.com>
2025-03-12 08:46:49 +0330

    faktor

--- router/mainApi.js
+++ router/mainApi.js
@@ -170,7 +170,7 @@ router.get('/get-sekke', async (req,res)=>{
 router.post('/get-product', async (req,res)=>{
     const id = req.body.id
     try{
-        result = await updateProduct(id,id,query)
+        result = await updateProduct(id,id,1)
         res.json({result})
     }
     catch(error){
